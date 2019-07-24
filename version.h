69fb21c34880a5ea9564ae7b21221e4130fe9aa9
Jakub Ďuraš <jakub@duras.me>
2019-07-25 00:10:20 +0200

    Format nssm.cpp and change error status codes

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
-#define NSSM_VERSION _T("2.23-106-g9b37719")
-#define NSSM_VERSIONINFO 2,23,106,0
-#define NSSM_DATE _T("Tue 07/16/2019")
+#define NSSM_VERSION _T("2.23-107-gfefeb61")
+#define NSSM_VERSIONINFO 2,23,107,0
+#define NSSM_DATE _T("Wed 07/17/2019")
 #define NSSM_FILEFLAGS VS_FF_PRERELEASE
 #define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-")
